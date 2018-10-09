4e42a4ed475bb01073f2f78a946c638a71f5061d
JaakTepandi <34964045+JaakTepandi@users.noreply.github.com>
2018-10-09 18:26:20 +0300

    Update cpc.py

--- cpc.py
+++ cpc.py
@@ -1,2 +1,21 @@
-def cp(filename):
-    return "removed"
+class dc:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_filename(self):
+        return self.name+".csv" 
+            
+    def raw_file(self):
+        import pandas as pd
+        return pd.read_csv(self.get_filename())
+
+    def file_w_headers(self):
+        f=self.raw_file()
+        f.columns = ['year', 'rank', 'company', 'revenue', 'profit']
+        return f
+
+    def file_ok(self):
+        f=self.file_w_headers()
+        return f.profit.dtype == 'float64'
+
